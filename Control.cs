e08fba6de2cd9e500d9443736ee1be76f2c754b3
“Adam <vip.phamquangthinh@gmail.com>
2025-01-14 00:47:42 +0700

    Add File Dialog

--- Control.cs
+++ Control.cs
@@ -3,12 +3,13 @@ using System;
 
 public partial class Control : Godot.Control
 {
-	LineEdit pathFolder; Button chooseFolder;
+	LineEdit pathFolder; Button chooseFolder; FileDialog FileDialog;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		pathFolder = GetNode<LineEdit>("./pathFolder");
 		chooseFolder = GetNode<Button>("./chooseFolder");
+		FileDialog = GetNode<FileDialog>("./FileDialog");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,7 +19,27 @@ public partial class Control : Godot.Control
 
 	private void _on_button_pressed()
 	{
-		
-
+		FileDialog.Popup();
 	}
+	private void _on_file_dialog_dir_selected(string path)
+	{
+		pathFolder.Text = ShortenPath(path);
+	}
+	private string ShortenPath(string path, int maxLength = 30)
+	{
+		if (path.Length <= maxLength)
+		{
+			return path;
+		}
+
+		int charsToShow = maxLength / 2;
+		string start = path.Substring(0, charsToShow);
+		string end = path.Substring(path.Length - charsToShow);
+
+		return $"{start}...{end}";
+	}
+
 }
+
+
+
